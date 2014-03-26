8146ca077a4bce52961cc9de44a0292033ac4d11
Brinon <pablo.llueca@gmail.com>
2014-03-26 16:33:04 +0100

    working on fork

--- zeos/sys.c
+++ zeos/sys.c
@@ -28,7 +28,7 @@ int check_fd(int fd, int permissions)
 
 int sys_ni_syscall()
 {
-	return -38; /*ENOSYS*/
+  return -38; /*ENOSYS*/
 }
 
 
@@ -37,29 +37,29 @@ int sys_write(int fd,char * buffer, int size)
   if(check_fd(fd, 1) >= 0)
     {
       if(buffer != NULL) {
-	if(size >= 0)
-	  {  //tot ok
-	    int written = 0;
+        if(size >= 0)
+          {  //tot ok
+            int written = 0;
 	  
-	    while(size >= MAX_WRITE)
-	      {
-		char buff2[MAX_WRITE];
-		if(copy_from_user(buffer + written, &buff2, MAX_WRITE) >= 0)
-		  {
-		    int w = sys_write_console(&buff2, MAX_WRITE);
-		    written += w;
-		    size -= w;
-		  }
-	      }	  
-	    char buff2[size];  //size menor q max_write
-	    if(copy_from_user(buffer + written, &buff2, size) >= 0){
-	      int w = sys_write_console(&buff2, size);
-	      written += w;
-	      size -= w;
-	    }
-	    if(size == 0) return written;  //tot ok
-	  }
-	return -22; //size< 0
+            while(size >= MAX_WRITE)
+              {
+                char buff2[MAX_WRITE];
+                if(copy_from_user(buffer + written, &buff2, MAX_WRITE) >= 0)
+                  {
+                    int w = sys_write_console(&buff2, MAX_WRITE);
+                    written += w;
+                    size -= w;
+                  }
+              }	  
+            char buff2[size];  //size menor q max_write
+            if(copy_from_user(buffer + written, &buff2, size) >= 0){
+              int w = sys_write_console(&buff2, size);
+              written += w;
+              size -= w;
+            }
+            if(size == 0) return written;  //tot ok
+          }
+        return -22; //size< 0
       }
       return -14; //not null
     }
@@ -88,14 +88,25 @@ int ret_from_fork()
   return 0;
 }
 
+
+/*
+ * crea un proceso hijo identico al padre
+ * el retorno de fork() sera 0 para el proceso hijo y el pid del nuevo proceso para el padre
+ * sketch:
+ * - adquirir un PCB libre de la freequeue
+ * - heredar la kernel stack del padre
+ * - copiar el espacio de memoria del padre al hijo (solo data+stack, codigo es compartido)
+ * - al entrar en ejecucion el hijo ha de pasar por "ret_from_fork" para q retorne 0, y asi identificarlo como el proceso hijo
+ * -
+ */
 int sys_fork()
 {
-  int PID, child_frame, allocDir_ret;
+  int PID, child_frame,
+    allocDir_ret, i, dir_ini, dir_dest;
   list_head *l;
   task_struct *child, *parent;
   page_table_entry *child_page, *parent_page;
   parent = current();
-
   // get a free PCB for the child
   if(list_empty(&free_queue) == 0){  //no sta buida
     l = list_first(&free_queue);
@@ -103,39 +114,44 @@ int sys_fork()
     child = list_head_to_task_struct(l);
 
     // inheritance of parent kernel stack
-    copy_data(parent,child, KERNEL_STACK_SIZE*4);    
-    allocDir_ret = allocate_DIR(child);
-    if(allocDir_ret != 1){    
-      child_frame = alloc_frame();
-      if(child_frame >= 0){
-	/*
-	 * cal copiar la regio de codi i de data+stack del pare al fill
-	 * com no es pot accedir, primer shan de mapejar les pagines del
-	 * fill a la page table entry del fare, usant set_ss_pag i alliberantles despres
-	 * amb del_ss_pag
-	 */
-	child_page = get_PT(child);
-	set_ss_pag(parent_page, "?" ,child_frame); //
-	copy_data();
-	del_ss_pag(parent_page, "?");
-	
-	PID = PID_MAX + 1;  // no hay PIDs repetidos
-	PID_MAX = PID;
-
-	child->kernel_esp = &ret_from_fork; // el hijo empezara a ejecutarse saliendo de fork con valor de retorn 0
-	list_add_tail(child.list, &freequeue);  
-	return PID;
+    copy_data(parent,child, KERNEL_STACK_SIZE*4);
+    child_page = get_PT(child);
+    parent_page = get_PT(parent);
+    if(allocDir_ret != 1){
+      for(i = 0; i < NUM_PAG_DATA; ++i){
+        child_frame = alloc_frame();
+        if(child_frame >= 0){
+          set_ss_pag(child_page,i,child_frame);
+          set_ss_pag(parent_page, i, child_frame);  // se liberara al acabar
+        }
+        else{
+          //no quedan frames
+        }
       }
-      else{
-	/* no frame */ 
-      }    
+      //se han mapeado todos los frames dnd tocan
+      dir_ini = KERNEL_START + (NUM_PAG_KERNEL * PAGE_SIZE);
+      dir_dest = dir_ini + (NUM_PAG_CODE + NUM_PAG_DATA) * PAGE_SIZE; //en mem logica
+      copy_data(dir_ini, dir_dest, NUM_PAG_DATA * PAGE_SIZE);
+
+      for(i = 0; i < NUM_PAG_DATA; ++i){
+        del_ss_pag(parent_page, i);  //se liberan los frames del hijo mapeados en la tabla de paginas del padre
+      }
+      PID = PID_MAX + 1;  // no hay PIDs repetidos
+      PID_MAX = PID;
+      child -> PID = PID;
+      /*
+       * el hijo empezara a ejecutarse saliendo de fork con valor de retorn 0
+       * child->kernel_esp = &ret_from_fork;
+       * list_add_tail(child.list, &freequeue);
+       */
+      return PID;
     }
     else{
       /* no directory allocated */
     }
   }
   else{
-      /* no free pcb */
+    /* no free pcb */
   }
   return PID;
 }
