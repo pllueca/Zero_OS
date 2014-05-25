0d81f2f6d0d8a34a063c8eaedede819da47388e9
Brinon <pablo.llueca@gmail.com>
2014-05-25 20:43:55 +0200

    experiments

--- sockets/exec1.sh
+++ sockets/exec1.sh
@@ -2,6 +2,5 @@
 for i in 1 10 50 100 200 300 500 750 1000
 do
     ./clientSocket $i localhost 5000
-#        ./launchClient 1 $i localhost 5000 p$i
 done        
 
--- sockets/exec2.sh
+++ sockets/exec2.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
-for i in 1 10 50 100 200 300 500 750 999
+#make clean
+#make
+echo "introdueix un nom per el directori\n"
+read experiments
+mkdir $experiments
+for i in 1 10 50 100 200 250 #300 500 750 999
 do
-       ./launchClient $i 100 localhost 5000 p$i
+    for j in 1 50 200 300 500 750 1000 2000
+    do
+	# i = num clients, j = num iteracions
+	echo "$i clients i $j iteracions\n"
+	./launchClient $i $j localhost 5000 $experiments/prova-$i-$j
+    done
 done        
