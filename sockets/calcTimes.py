61e659612a80206d786657ee29c65cb2fdd79fc1
Brinon <pablo.llueca@gmail.com>
2014-05-29 15:53:12 +0200

    calctimes

--- sockets/calcTimes.py
+++ sockets/calcTimes.py
@@ -18,8 +18,29 @@ def suma_Exec(nom):
     avg = s / numClients
     return s
 
-print dirBase + 'p1'
-print num_files('p1')
-print suma_Exec('p1')
+
+# a l'arxiu f nomes hi ha linies 'Time 123123'
+# retorna la mitjana del temps
+def suma_temps(nom):
+    s = 0
+    n = 0
+    f = op
 
 
+
+def calc(k,i,j):
+    s = "prova-"+str(k)+"-"+str(i)+"-"+str(j)
+    f = open(s+"/resumTimes.txt","rw")
+    x = suma_Exec(s)
+    f.write('Average Time: '+str(x)+'\n')
+
+
+def main():
+    for i in [1, 10, 50, 100, 200, 250]:
+        for j in [1, 50, 200, 300, 500, 750, 1000, 2000]:
+            for k in [1,2,3]:
+                calc(k,i,j)
+    print "End!\n"
+
+if __name__ == "__main__":
+    main()
